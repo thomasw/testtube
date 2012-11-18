bbcd5588545f6c960589bc158e447b1f09841317
Thomas W <thomas@matchstrike.net>
2012-11-18 14:33:47 -0800

    Update setup.py description

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ setup(
     url='https://github.com/thomasw/testtube',
     author=__author__,
     author_email='thomas.welfley+testtube@gmail.com',
-    description='',
+    description='Testtube watches a specified directory for file changes '
+                'and runs a set of defined tests against those files whenever'
+                ' a chance occurs.',
     packages=find_packages(),
     scripts=['testtube/bin/stir'],
     classifiers=[
