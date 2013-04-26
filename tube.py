58e5400dca75954aad35379af809d50d3b64fff1
Justin Blake <justin@hentzia.com>
2013-04-26 18:02:53 -0400

    Don't let pyc files trigger testtube runs.

--- tube.py
+++ tube.py
@@ -1,5 +1,5 @@
 from testtube.helpers import pep8_all, pyflakes_all, nosetests_all
 
 PATTERNS = (
-    (r'.*\.py', [pep8_all, pyflakes_all, nosetests_all]),
+    (r'.*\.py$', [pep8_all, pyflakes_all, nosetests_all]),
 )
