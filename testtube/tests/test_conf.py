ab62a6be8519310111f527aa6437e11a62f793a2
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-18 00:05:57 +0200

    Revert "Rm redundant docstrings"
    This reverts commit 4270d4b21cc53366452fd3d0e75a5b979c53bb26.

--- testtube/tests/test_conf.py
+++ testtube/tests/test_conf.py
@@ -9,12 +9,15 @@ from testtube import conf
 
 class ConfModule(unittest2.TestCase):
     def test_set_src_dir_should_set_the_src_dir(self):
+        """_set_src_dir method should set conf.SRC_DIR"""
         conf._set_src_dir('bar/')
         self.assertEqual(
             conf.SRC_DIR, os.path.join(os.getcwd(), 'bar'))
 
     @patch('__builtin__.__import__')
     def test_get_test_suite_from_settings_should_grab_settings(self, settings):
+        """_get_test_suite_from_settings should grab the appropriate settings
+        from the passed settings module"""
         # Create a fake settings module and make it the return value for
         # our __import__ mock
         fake_settings = imp.new_module('mysettings')
