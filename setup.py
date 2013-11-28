e5288928f382e9bcd3fd38a578103b0233eeadc6
Michael Joseph <michaeljoseph+github@gmail.com>
2013-11-28 13:23:33 +0200

    Classifiers

--- setup.py
+++ setup.py
@@ -36,6 +36,13 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
     test_suite='nose.collector',
 )
