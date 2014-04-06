e30b9cfd55b91424de62e5ac9fcdb0464a78f37e
Thomas W <thomas@matchstrike.net>
2014-04-05 21:11:23 -0700

    Make import mock.Mock or unittest.mock.Mock easier

--- testtube/tests/__init__.py
+++ testtube/tests/__init__.py
@@ -1,6 +1,16 @@
 import sys
 
 if sys.version_info[:2] < (2, 7):
-    import unittest2 as unittest
+    import unittest2 as unittest  # NOQA
 else:
-    import unittest
+    import unittest  # NOQA
+
+if sys.version_info < (3,):
+    from mock import Mock, patch  # NOQA
+else:
+    from unittest.mock import Mock, patch  # NOQA
+
+
+# Frosted doesn't yet support noqa flags, so this hides the imported/unused
+# complaints
+Mock, patch, unittest
