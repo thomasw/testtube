b33fe9896cb4db8d22fd78f72ed5e688719dc0cb
Thomas W <thomas@matchstrike.net>
2014-04-05 21:07:08 -0700

    Abandon `python setup.py test` in favor of `nosetests`
    There's too much hacky nonsense required to get this to work correctly.

--- setup.py
+++ setup.py
@@ -1,23 +1,8 @@
 from setuptools import setup, find_packages
 
 from testtube import __version__, __author__
-import sys
 
 install_requires = ['watchdog==0.6.0']
-
-tests_require = [
-    'nose==1.2.1',
-    'pep8==1.3.3',
-    'pinocchio==0.3.1',
-    'pyflakes==0.5.0',
-]
-# Add Python 2.6-specific dependencies
-if sys.version_info[:2] < (2, 7):
-    tests_require.append('unittest2==0.5.1')
-
-# Add Python 2.6 and 2.7-specific dependencies
-if sys.version < '3':
-    tests_require.append('mock==1.0.1')
 
 setup(
     name='testtube',
@@ -31,7 +16,6 @@ setup(
                 'a change occurs.',
     packages=find_packages(),
     scripts=['testtube/bin/stir'],
-    tests_require=tests_require,
     install_requires=install_requires,
     classifiers=[
         'Intended Audience :: Developers',
