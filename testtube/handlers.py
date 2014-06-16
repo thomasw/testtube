140882559ae56b3fc9c0431676bcd76afc38711c
Thomas W <thomas@matchstrike.net>
2014-06-15 23:52:45 -0700

    pep257 fixes

--- testtube/handlers.py
+++ testtube/handlers.py
@@ -4,6 +4,9 @@ from testtube.runner import run_tests
 
 
 class PyChangeHandler(FileSystemEventHandler):
+
     """Watchdog handler that executes the test runner on file changes."""
+
     def on_any_event(self, event):
+        """Execute the test suite whenever files change."""
         run_tests(event.src_path)
